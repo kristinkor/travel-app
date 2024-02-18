6212dc337986bfe760b40557c344823037b09589
korzhenevskaya <kristinkor@gmail.com>
2024-02-17 21:00:15 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { pageLinks, socialLinks } from '../data'
+import SocialLink from './SocialLink'
+import PageLinks from './PageLinks'
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          return <SocialLink key={link.id} {...link} itemClass="footer-icon" />
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Kristina travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/data.js
@@ -0,0 +1,157 @@
+import image1 from './images/image1.jpg'
+import image2 from './images/image2.jpg'
+import image3 from './images/image3.jpeg'
+import image4 from './images/image4.jpg'
+import image5 from './images/image5.jpg'
+import image6 from './images/image6.jpg'
+import image7 from './images/image7.jpg'
+import image8 from './images/image8.jpg'
+
+export const pageLinks = [
+  {
+    id: 1,
+    href: '#home',
+    text: 'home',
+  },
+  {
+    id: 2,
+    href: '#about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    href: '#services',
+    text: 'services',
+  },
+  {
+    id: 4,
+    href: '#tours',
+    text: 'tours',
+  },
+]
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: 'https://www.twitter.com',
+    icon: 'fab fa-facebook',
+  },
+  {
+    id: 2,
+    href: 'https://www.twitter.com',
+    icon: 'fab fa-twitter',
+  },
+  {
+    id: 3,
+    href: 'https://www.twitter.com',
+    icon: 'fab fa-instagram',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-plane fa-fw',
+    title: 'Flight Booking',
+    text: 'Book your flights with ease and convenience.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-hotel fa-fw',
+    title: 'Hotel Reservations',
+    text: 'Find and book hotels for a comfortable stay.',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-map-marker fa-fw',
+    title: 'Tour Packages',
+    text: 'Explore exciting destinations with our curated tour packages.',
+  },
+  {
+    id: 4,
+    icon: 'fas fa-car fa-fw',
+    title: 'Car Rentals',
+    text: 'Rent a car for seamless transportation during your trip.',
+  },
+  {
+    id: 5,
+    icon: 'fas fa-camera fa-fw',
+    title: 'Photography Tours',
+    text: 'Capture memories with guided photography tours.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: image1,
+    date: '2023-05-15',
+    title: 'Explore the UK',
+    location: 'United Kingdom',
+    duration: 7,
+    cost: '$1200',
+  },
+  {
+    id: 2,
+    image: image2,
+    date: '2023-07-20',
+    title: 'Discover the USA',
+    location: 'United States',
+    duration: 10,
+    cost: '$2500',
+  },
+  {
+    id: 3,
+    image: image3,
+    date: '2023-09-10',
+    title: 'Yellowstone Adventure',
+    location: 'USA',
+    duration: 5,
+    cost: '$800',
+  },
+  {
+    id: 4,
+    image: image4,
+    date: '2024-01-28',
+    title: 'Journey through Georgia',
+    location: 'Georgia',
+    duration: 12,
+    cost: '$1800',
+  },
+  {
+    id: 5,
+    image: image5,
+    date: '2024-04-05',
+    title: 'Slovakia Expedition',
+    location: 'Slovakia',
+    duration: 8,
+    cost: '$1500',
+  },
+  {
+    id: 6,
+    image: image6,
+    date: '2023-12-12',
+    title: 'Discover Poland',
+    location: 'Poland',
+    duration: 6,
+    cost: '$1000',
+  },
+  {
+    id: 7,
+    image: image7,
+    date: '2024-03-18',
+    title: 'Lithuania Discovery',
+    location: 'Lithuania',
+    duration: 9,
+    cost: '$1200',
+  },
+  {
+    id: 8,
+    image: image8,
+    date: '2023-06-25',
+    title: 'Swedish Escape',
+    location: 'Sweden',
+    duration: 5,
+    cost: '$900',
+  },
+]
